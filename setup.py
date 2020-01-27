860d56b33f6b979df53825dad4529845defe5d86
Seg30 <60166529+Seg30@users.noreply.github.com>
2020-01-27 07:05:27 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
 
     install_requires=[
         'scikit-learn==0.21.2',
-        'pandas==0.25.0',
+        'pandas==0.25.3',
         'ZigZag==0.2.2',
         'matplotlib==3.1.1',
     ],
